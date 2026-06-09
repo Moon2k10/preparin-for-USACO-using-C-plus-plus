92675e7615efc515ca5ed2807ad9413a1a868ce2
Moon2k10 <anthu26102010@gmail.com>
2026-06-09 09:12:20 +0700

    Add age condition check in bai1_condition.cpp

---
+++ bai1_condition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//--- Bai 1: Condition ---
+int main() {
+  int age = 25;
+  if (age >= 18) {
+  cout << "Allowed";
+  } else {
+  cout << "Not Allowed";
+  } 
+  return 0;
+}
