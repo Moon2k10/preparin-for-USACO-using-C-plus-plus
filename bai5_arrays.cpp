aa0a944abf3fe8f54e33256a991764566c502e16
Moon2k10 <anthu26102010@gmail.com>
2026-06-14 00:27:05 +0700

    Create bai5_arrays.cpp

---
+++ bai5_arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int numbers [5];
+  numbers [0] = 10;
+  numbers [4] = 50;
+  cout << numbers [0] << "\n";
+  cout << numbers [4] << "\n";
+  return 0;
+}
